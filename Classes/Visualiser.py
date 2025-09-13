0b79d6397cc66758e46a1d17391cf083cfc92f1f
oscar d <oscar.djuric@gmail.com>
2025-09-13 16:11:00 +0100

    Graphing multiple tickrs at the same time

--- Classes/Visualiser.py
+++ Classes/Visualiser.py
@@ -2,42 +2,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+availableColumns = ['Close', 'High', 'Low', 'Open', 'Volume']
+
 class Visualiser:
     
-    def __init__(self, assetType='Stocks', tickr='AMD', timeRes='1d'):
+    def __init__(self, assetType='Stocks', tickr=['AMD'], timeRes='1d'):
 
         self.assetType = assetType
         self.tickr = tickr
         self.timeRes = timeRes
 
         currentDirectory = os.getcwd()
-        self.filePath = currentDirectory + '//Historical Data//' + assetType + '//' + tickr + '_' + timeRes + '_01012010_01012025.csv'
 
-        self.df = pd.read_csv(self.filePath, header=0).iloc[2:]
-        self.df['Price'] = pd.to_datetime(self.df['Price'], format="%Y-%m-%d")
+        self.df = []
+        for i in range(len(tickr)):
+            filePath = currentDirectory + '//Historical Data//' + assetType + '//' + tickr[i] + '_' + timeRes + '_01012010_01012025.csv'
+            self.df.append(pd.read_csv(filePath, header=0).iloc[2:])
+            self.df[i]['Price'] = pd.to_datetime(self.df[i]['Price'], format="%Y-%m-%d")
         
-        for col in self.df.columns:
-            if col != 'Price':
-                self.df[col] = pd.to_numeric(self.df[col], errors='coerce')
-
-
+            for col in self.df[i].columns:
+                if col != 'Price':
+                    self.df[i][col] = pd.to_numeric(self.df[i][col], errors='coerce')
 
     def plotPrice(self, priceType='Open', startDate=None, endDate=None):
-        if priceType not in self.df.columns:
-            raise ValueError(f"Column '{priceType}' not found. Available: {list(self.df.columns)}")
+
+        if priceType not in availableColumns:
+            print('price type not available - yet!')
 
         data = self.df.copy()
-        if startDate:
-            data = data[data['Price'] >= pd.to_datetime(startDate, dayfirst=True)]
-        if endDate:
-            data = data[data['Price'] <= pd.to_datetime(endDate, dayfirst=True)]
-
-        title = self.tickr + ": " + priceType
-        
         plt.figure(figsize=(10, 5))
-        plt.plot(data['Price'], data[priceType])
         plt.xlabel("Date")
         plt.ylabel(f"{priceType}")
-        plt.title(title)
+
+        for i in range(len(self.tickr)):
+            if startDate:
+                data[i] = data[i][data[i]['Price'] >= pd.to_datetime(startDate, dayfirst=True)]
+            if endDate:
+                data[i] = data[i][data[i]['Price'] <= pd.to_datetime(endDate, dayfirst=True)]
+        
+            plt.plot(data[i]['Price'], data[i][priceType], label=self.tickr[i])
+
+        plt.title(priceType)
         plt.grid()
+        plt.legend()
         plt.show()
--- test bench.py
+++ test bench.py
@@ -1,7 +1,8 @@
 #%%
 from Classes.Visualiser import Visualiser
 
-stock = Visualiser('Stocks', 'NVDA', '1d')
-stock.plotPrice(priceType='High',
-                startDate="01/01/2024",
+tickrs = ['AMZN', 'NVDA', 'DIS']
+stock = Visualiser('Stocks', tickrs, '1d')
+stock.plotPrice(priceType='Close',
+                startDate="01/01/2018",
                 endDate="31/12/2024")
