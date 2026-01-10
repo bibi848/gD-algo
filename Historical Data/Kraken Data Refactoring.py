945e17c7163985a6bf7df992ac3216fb4ba8ba9a
oscar d <oscar.djuric@gmail.com>
2026-01-10 18:29:32 +0000

    Refactoring Kraken Data

---
+++ Historical Data/Kraken Data Refactoring.py
@@ -0,0 +1,46 @@
+#%%
+'''
+This code takes each of the coin-pairs downloaded from Kraken and refactors them to the
+correct format. The original data has 3 columns: time [s] - price - amount. 
+The new data has the format: Timestamp - Open - High - Low - Close - Volume in minute-by-minute
+resolution. The new data is stored in the Kraken_Data_Refactored folder. 
+'''
+
+import os
+import pandas as pd
+
+coinPairs = ['ETHEUR', 'ETHGBP', 'ETHUSD',
+             'SOLEUR', 'SOLGBP', 'SOLUSD',
+             'XBTEUR', 'XBTGBP', 'XBTUSD',
+             'XRPEUR', 'XRPGBP', 'XRPUSD']
+
+INPUT_DIR  = "Kraken_Data"
+OUTPUT_DIR = "Kraken_Data_Refactored"
+TIMEFRAME  = "1min"
+
+def load_and_resample(pair):
+    path = os.path.join(INPUT_DIR, f"{pair}.csv")
+    print(f"Processing: {pair}")
+
+    df = pd.read_csv(path)
+    df.columns = ['timestamp', 'price', 'volume']
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s', utc=True)
+
+    df = df.sort_values('timestamp')
+    df.set_index('timestamp', inplace=True)
+
+    # Resample to 1-minute OHLC
+    ohlc = df['price'].resample(TIMEFRAME).ohlc()
+    vol = df['volume'].resample(TIMEFRAME).sum()
+
+    ohlcv = ohlc.join(vol)
+    ohlcv.columns = ['open', 'high', 'low', 'close', 'volume']
+
+    ohlcv.dropna(inplace=True)
+
+    return ohlcv
+
+for pair in coinPairs:
+    ohlcv = load_and_resample(pair)
+    out_path = os.path.join(OUTPUT_DIR, f"{pair}_1m.csv")
+    ohlcv.to_csv(out_path)
--- test bench.py
+++ test bench.py
@@ -1,10 +1,10 @@
 #%%
 from Classes.Visualiser import Visualiser
 
-tickrs = ['DIS']
-stock = Visualiser('Stocks', tickrs, '1d')
+tickrs = ['BTC-USD']
+stock = Visualiser('Crypto', tickrs, '1d', startDate='01012015', endDate='01012026')
 stock.plotPrice(priceType='Close',
                 percentage=True,
-                MA=[200, 50, 20],
-                startDate="2020-01-01",
-                endDate="2021-01-01")
+                MA=[200, 50],
+                startDate="2017-01-01",
+                endDate="2026-01-01")
